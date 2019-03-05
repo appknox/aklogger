f0a34a9a1ea7fdbd3d9aafd350f634df33c90893
Shrikrishna Singh <krishnasingh.ss30@gmail.com>
2019-03-05 17:19:44 +0530

    Modify handlers

--- varys/handlers.py
+++ varys/handlers.py
@@ -1,69 +1,120 @@
+import logging
+
 import json
 import traceback
-from logging import (
-    Handler,
-    StreamHandler,
-    FileHandler,
-    CRITICAL,
-    ERROR,
-    WARNING,
-    INFO,
-    FATAL,
-    DEBUG,
-    NOTSET,
-    Formatter)
+from logging import CRITICAL, ERROR, WARNING, INFO, FATAL, DEBUG, NOTSET, Formatter
 
-from django.utils.log import AdminEmailHandler
-from slacker_log_handler import SlackerLogHandler as SlackLogHandler
+import six
+import slacker
+
+ERROR_COLOR = 'danger'  # color name is built in to Slack API
+WARNING_COLOR = 'warning'  # color name is built in to Slack API
+INFO_COLOR = '#439FE0'
+
+COLORS = {
+    CRITICAL: ERROR_COLOR,
+    FATAL: ERROR_COLOR,
+    ERROR: ERROR_COLOR,
+    WARNING: WARNING_COLOR,
+    INFO: INFO_COLOR,
+    DEBUG: INFO_COLOR,
+    NOTSET: INFO_COLOR,
+}
+
+DEFAULT_EMOJI = ':heavy_exclamation_mark:'
 
 
-class SlackerLogHandler(SlackLogHandler):
+class NoStacktraceFormatter(Formatter):
+    """
+    By default the stacktrace will be formatted as part of the message.
+    Since we want the stacktrace to be in the attachment of the Slack message,
+     we need a custom formatter to leave it out of the message
+    """
 
-    def slack_alert(self, summary, details, channel, color='good'):
-        if settings.DISABLE_SLACK:
-            return
-        if not settings.DEBUG:
-            slkr = slacker.Slacker(settings.SLACK_TOKEN)
-            slkr.chat.post_message(
-                channel=channel,
-                text=summary,
-                username='Mycroft',
-                icon_emoji=':squirrel:',
-                attachments=[
-                    {
-                        'fallback': summary,
-                        'text': details,
-                        'color': color,
-                    }
-                ] if details else None,
+    def formatException(self, ei):
+        return None
+
+
+class SlackerLogHandler(logging.Handler):
+    def __init__(self, api_key, channel, stack_trace=True, username='Python logger', icon_url=None, icon_emoji=None,
+                 fail_silent=False, ping_users=None, ping_level=None):
+        logging.Handler.__init__(self)
+        self.formatter = NoStacktraceFormatter()
+
+        self.stack_trace = stack_trace
+        self.fail_silent = fail_silent
+
+        self.slacker = slacker.Slacker(api_key)
+
+        self.username = username
+        self.icon_url = icon_url
+        self.icon_emoji = icon_emoji if (icon_emoji or icon_url) else DEFAULT_EMOJI
+        self.channel = channel
+        if not self.channel.startswith('#') and not self.channel.startswith('@'):
+            self.channel = '#' + self.channel
+
+        self.ping_level = ping_level
+        self.ping_users = []
+
+        if ping_users:
+            user_list = self.slacker.users.list().body['members']
+
+            for ping_user in ping_users:
+                ping_user = ping_user.lstrip('@')
+
+                for user in user_list:
+                    if user['name'] == ping_user:
+                        self.ping_users.append(user['id'])
+                        break
+                else:
+                    raise RuntimeError('User not found in Slack users list: %s' % ping_user)
+
+
+    def build_msg(self, record):
+        return six.text_type(self.format(record))
+
+
+    def build_trace(self, record, fallback):
+        trace = {
+            'fallback': fallback,
+            'color': COLORS.get(self.level, INFO_COLOR)
+        }
+
+        if record.exc_info:
+            trace['text'] = '\n'.join(traceback.format_exception(*record.exc_info))
+
+        return trace
+
+
+    def emit(self, record):
+        message = self.build_msg(record)
+
+        if self.ping_users and record.levelno >= self.ping_level:
+            for user in self.ping_users:
+                message = '<@%s> %s' % (user, message)
+
+        if self.stack_trace:
+            trace = self.build_trace(record, fallback=message)
+            attachments = json.dumps([trace])
+        else:
+            attachments = None
+
+        try:
+            self.slacker.chat.post_message(
+                text=message,
+                channel=self.channel,
+                username=self.username,
+                icon_url=self.icon_url,
+                icon_emoji=self.icon_emoji,
+                attachments=attachments,
             )
+        except slacker.Error as e:
+            if self.fail_silent:
+                pass
+            else:
+                raise e
 
 
-    def slack_error(self, summary, details=None):
-        logger.error(summary)
-        if details:
-            logger.error(details)
-        slack_alert(
-            summary, details, channel=settings.SLACK_ERROR_CHANNEL, color='danger')
-
-
-    def slack_warning(self, summary, details=None):
-        logger.warning(summary)
-        if details:
-            logger.warning(details)
-        slack_alert(
-            summary, details, channel=settings.SLACK_ERROR_CHANNEL, color='warning')
-
-
-    def slack_info(self, summary, details=None):
-        logger.info(summary)
-        if details:
-            logger.info(details)
-        slack_alert(
-            summary, details, channel=settings.SLACK_ERROR_CHANNEL, color='good')
-
-
-FileHandler = FileHandler
-StreamHandler = StreamHandler
-AdminEmailHandler = AdminEmailHandler
+FileHandler = logging.FileHandler
+StreamHandler = logging.StreamHandler
 SlackerLogHandler = SlackerLogHandler
