e41e6d808f109015498acfd586b417c39245f765
Shrikrishna Singh <krishnasingh.ss30@gmail.com>
2019-03-11 11:48:19 +0530

    Update to new structure of aklogger

--- aklogger/__init__.py
+++ aklogger/__init__.py
@@ -1,57 +1,162 @@
 import logging
-from . import formatters, handlers, utils
+
+from slacker import Slacker
+
+tpl = """
+=====================
+{summary}
+---------------------
+{details}
+=====================
+"""
+
+root_logger = logging.getLogger('root')
+root_stream_handler = logging.StreamHandler()
+
+try:
+    from celery.utils.log import get_task_logger
+    root_logger = get_task_logger('root')
+except ImportError:
+    pass
+
+root_logger.addHandler(root_stream_handler)
+
+try:
+    from celery.signals import after_setup_task_logger, after_setup_logger
+
+    @after_setup_logger.connect
+    def after_setup_aklogger(logger, *args, **kwargs):
+        root_logger.removeHandler(root_stream_handler)
+
+    @after_setup_task_logger.connect
+    def after_setup_task_aklogger(logger, *args, **kwargs):
+        root_logger.removeHandler(root_stream_handler)
+except ImportError:
+    pass
+
+
+def get_logger(name):
+    return root_logger.getChild(name)
+
+
+CRITICAL = logging.CRITICAL
+ERROR = logging.ERROR
+WARNING = logging.WARNING
+INFO = logging.INFO
+DEBUG = logging.DEBUG
+NOTSET = logging.NOTSET
+
+_levelToName = {
+    CRITICAL: 'CRITICAL',
+    ERROR: 'ERROR',
+    WARNING: 'WARNING',
+    INFO: 'INFO',
+    DEBUG: 'DEBUG',
+    NOTSET: 'NOTSET',
+}
 
 
 class AKLogger(object):
 
-    def __init__(self, name='root', level=None, **kwargs):
-        self.logger = logging.Logger(name)
-        self.handler = kwargs.get('handler', handlers.StreamHandler())
-        self.formatter = kwargs.get('formatter', formatters.basic_formatter)
-        if not level:
-            self.level = 'DEBUG'
-        self.handler.setLevel(self.level)
-        self.handler.setFormatter(self.formatter)
-        self.logger.addHandler(self.handler)
+    def __init__(self, name, parent=None):
+        self.set_name(name)
+        self.parent = parent
+        self.logger = get_logger(self.get_name())
+        self.slkr = None
+        self.slack_token = None
+        self.setLevel(WARNING)
 
-    def set_level(self, level):
-        return self.logger.setLevel(level)
+    def set_name(self, name):
+        self.name = name
 
-    def debug(self, title=None, msg=None, *args, **kwargs):
-        message = utils.get_message(title, msg)
-        if not message:
+    def get_name(self):
+        if self.parent is not None:
+            return self.parent.get_name() + ':' + self.name
+        return self.name
+
+    def getLogger(self, name):
+        return AKLogger(name, parent=self)
+
+    def setLevel(self, level):
+        self.level = logging._checkLevel(level)
+        self.logger.setLevel(self.level)
+
+    def enable_slack(self, token):
+        self.slkr = Slacker(token)
+        self.slkr.api.test()
+        self.slack_token = token
+
+    def disable_slack(self):
+        self.slkr = None
+        self.slack_token = None
+
+    def get_slack_level(self):
+        return self.slack_level or self.level
+
+    def set_slack_level(self, level):
+        self.slack_level = logging._checkLevel(level)
+
+    def log_to_file(self, file_name):
+        self.filename = file_name
+        handler = logging.FileHandler(self.filename)
+        self.logger.addHandler(handler)
+
+    def debug(self, summary, details=None, channel='#error',
+              *args, **kwargs):
+        self._log(DEBUG, summary, details, channel, *args, **kwargs)
+
+    def info(self, summary, details=None, channel='#error', *args,
+             **kwargs):
+        self._log(INFO, summary, details, channel, *args, **kwargs)
+
+    def warning(self, summary, details=None, channel='#error', *args,
+                **kwargs):
+        self._log(WARNING, summary, details, channel, *args, **kwargs)
+
+    def error(self, summary, details=None, channel='#error', *args,
+              **kwargs):
+        self._log(ERROR, summary, details, channel, *args, **kwargs)
+
+    def _log(self, level, summary=None, details=None, channel='#error',
+             *args, **kwargs):
+        msg = summary
+        if details:
+            msg = tpl.format(
+                summary=summary,
+                details=details
+            )
+        method_to_call = getattr(self.logger, _levelToName.get(level).lower())
+        method_to_call(msg, *args, **kwargs)
+        self.slack_alert(summary, details, channel, level)
+
+    def slack_alert(self, summary, details, channel, level,
+                    color='good'):
+        if not self.slkr:
             return
-        return self.logger.debug(message)
-
-    def info(self, title=None, msg=None, *args, **kwargs):
-        message = utils.get_message(title, msg)
-        if not message:
+        if level < self.slack_level:
             return
-        return self.logger.info(message)
+        try:
+            self.slkr.chat.post_message(
+                channel=channel,
+                text=summary,
+                username=self.name,
+                icon_emoji=':squirrel:',
+                attachments=[
+                    {
+                        'fallback': summary,
+                        'text': details,
+                        'color': color,
+                    }
+                ] if details else None,
+            )
+        except Exception as e:
+            self.logger.error('Slack failed: {}'.format(e))
 
-    def warning(self, title=None, msg=None, *args, **kwargs):
-        message = utils.get_message(title, msg)
-        if not message:
-            return
-        return self.logger.warning(message)
 
-    def error(self, title=None, msg=None, *args, **kwargs):
-        message = utils.get_message(title, msg)
-        if not message:
-            return
-        return self.logger.error(message)
+logger = AKLogger('root')
 
-    def critical(self, title=None, msg=None, *args, **kwargs):
-        message = utils.get_message(title, msg)
-        if not message:
-            return
-        return self.logger.critical(message)
 
-    def add_handler(self, hldr):
-        return self.logger.addHandler(hldr)
-
-    def remove_handler(self, hldr):
-        return self.logger.removeHandler(hldr)
-
-    def has_handlers(self):
-        return self.logger.hasHandlers()
+def getLogger(name=None):
+    if name is None:
+        return logger
+    return AKLogger(name)
--- aklogger/handlers.py
+++ aklogger/handlers.py
@@ -1,141 +1,23 @@
 import logging
-
-import json
-from logging import (
-    CRITICAL,
-    ERROR,
-    WARNING,
-    INFO,
-    FATAL,
-    DEBUG,
-    NOTSET,
-    Formatter,
-)
-
-import six
-import slacker
-
-from . import utils
-
-ERROR_COLOR = 'danger'  # color name is built in to Slack API
-WARNING_COLOR = 'warning'  # color name is built in to Slack API
-INFO_COLOR = '#439FE0'
-
-COLORS = {
-    CRITICAL: ERROR_COLOR,
-    FATAL: ERROR_COLOR,
-    ERROR: ERROR_COLOR,
-    WARNING: WARNING_COLOR,
-    INFO: INFO_COLOR,
-    DEBUG: INFO_COLOR,
-    NOTSET: INFO_COLOR,
-}
-
-DEFAULT_EMOJI = ':heavy_exclamation_mark:'
+from aklogger import logger
 
 
-class NoStacktraceFormatter(Formatter):
-    """
-    By default the stacktrace will be formatted as part of the message.
-    Since we want the stacktrace to be in the attachment of the Slack
-    message, we need a custom formatter to leave it out of the message.
-    """
+class DjangoHandler(logging.Handler):
 
-    def formatException(self, ei):
-        return None
+    def __init__(self, name, slack_token=None,
+                 slack_level=logging.ERROR, filename=None):
+        super().__init__()
+        logger.set_name(name)
+        if slack_token:
+            logger.enable_slack(slack_token)
+        logger.set_slack_level(slack_level)
+        if filename:
+            logger.log_to_file(filename)
 
-
-class SlackerLogHandler(logging.Handler):
-
-    def __init__(self, api_key, channel, stack_trace=True,
-                 username='Python logger', icon_url=None, icon_emoji=None,
-                 fail_silent=False, ping_users=None, ping_level=None):
-        logging.Handler.__init__(self)
-        self.formatter = NoStacktraceFormatter()
-
-        self.stack_trace = stack_trace
-        self.fail_silent = fail_silent
-
-        self.slacker = slacker.Slacker(api_key)
-
-        self.username = username
-        self.icon_url = icon_url
-        if (icon_emoji or icon_url):
-            self.icon_emoji = icon_emoji
-        else:
-            self.icon_emoji = DEFAULT_EMOJI
-        self.channel = channel
-        channel1 = self.channel.startswith('#')
-        channel2 = self.channel.startswith('@')
-        if not channel1 and not channel2:
-            self.channel = '#' + self.channel
-
-        self.ping_level = ping_level
-        self.ping_users = []
-
-        if ping_users:
-            user_list = self.slacker.users.list().body['members']
-
-            for ping_user in ping_users:
-                ping_user = ping_user.lstrip('@')
-
-                for user in user_list:
-                    if user['name'] == ping_user:
-                        self.ping_users.append(user['id'])
-                        break
-                else:
-                    raise RuntimeError(
-                        'User not found in Slack users list: %s' % ping_user)
-
-    def build_msg(self, record):
-        return six.text_type(self.format(record))
-
-    def build_trace(self, record, fallback, text):
-        trace = {
-            'fallback': fallback,
-            'text': text,
-            'color': COLORS.get(self.level, INFO_COLOR)
-        }
-        return trace
+    def setLevel(self, level):
+        logger.setLevel(level)
+        super(DjangoHandler, self).setLevel(level)
 
     def emit(self, record):
-        message = self.build_msg(record)
-        message_list = message.split(utils.MESSAGE_SEPERATOR, 1)
-        if len(message_list) == 2:
-            title = message_list[0]
-            description = message_list[1]
-        else:
-            title = message_list[0]
-            description = None
-
-        if self.ping_users and record.levelno >= self.ping_level:
-            for user in self.ping_users:
-                message = '<@{}> {}'.format(user, title)
-
-        if self.stack_trace:
-            trace = self.build_trace(record, fallback=title, text=description)
-            attachments = json.dumps([trace])
-        else:
-            attachments = None
-        try:
-            self.slacker.chat.post_message(
-                text=title,
-                channel=self.channel,
-                username=self.username,
-                icon_url=self.icon_url,
-                icon_emoji=self.icon_emoji,
-                attachments=attachments,
-            )
-        except slacker.Error as e:
-            if self.fail_silent:
-                pass
-            else:
-                raise e
-
-
-class FileHandler(logging.FileHandler):
-    pass
-
-
-class StreamHandler(logging.StreamHandler):
-    pass
+        method_to_call = getattr(logger, record.levelname.lower())
+        method_to_call(record.getMessage())
