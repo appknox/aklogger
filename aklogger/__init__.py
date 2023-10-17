1e30ff1096f57ab28469662b9dae943e428db4a1
Praseetha-KR <praseetha04@gmail.com>
2023-10-17 10:24:05 +0530

    Add typing

--- aklogger/__init__.py
+++ aklogger/__init__.py
@@ -68,7 +68,7 @@ _levelToColor = {
 
 
 class AKLogger(object):
-    def __init__(self, name, parent=None):
+    def __init__(self, name: str, parent: "AKLogger" = None) -> None:
         self.set_name(name)
         self.parent = parent
         self.logger = get_logger(self.get_name())
@@ -77,28 +77,29 @@ class AKLogger(object):
         self.slack_token = None
         self.slack_level = None
         self.slack_channel = None
+        self.slack_icon_emoji = ":shipit:"
 
         self.setLevel(NOTSET)
 
-    def set_name(self, name):
+    def set_name(self, name: str) -> None:
         self.name = name
 
-    def get_name(self):
+    def get_name(self) -> str:
         if self.parent is not None:
             return self.parent.get_name() + ":" + self.name
         return self.name
 
-    def getLogger(self, name):
+    def getLogger(self, name: str) -> "AKLogger":
         return AKLogger(name, parent=self)
 
-    def setLevel(self, level):
+    def setLevel(self, level: str) -> None:
         self.level = logging._checkLevel(level)
         self.logger.setLevel(self.level)
 
-    def getLevel(self):
+    def getLevel(self) -> str:
         return self.level
 
-    def enable_slack(self, token, channel):
+    def enable_slack(self, token: str, channel: str) -> None:
         self.slack_client = WebClient(token=token)
         api_response = self.slack_client.api_test()
         self.logger.debug(f"aklogger: Slack API test ok: {api_response['ok']}")
@@ -106,40 +107,40 @@ class AKLogger(object):
         self.slack_token = token
         self.slack_channel = channel
 
-    def disable_slack(self):
+    def disable_slack(self) -> None:
         self.slack_client = None
         self.slack_token = None
         self.slack_channel = None
 
-    def get_slack_level(self):
+    def get_slack_level(self) -> str:
         return self.slack_level or self.level
 
-    def set_slack_level(self, level):
+    def set_slack_level(self, level: str) -> None:
         self.slack_level = logging._checkLevel(level)
 
-    def set_slack_preferences(self, icon_emoji):
+    def set_slack_preferences(self, icon_emoji: str) -> None:
         self.slack_icon_emoji = icon_emoji
 
-    def log_to_file(self, file_name):
+    def log_to_file(self, file_name: str) -> None:
         self.filename = file_name
         handler = logging.FileHandler(self.filename)
         self.logger.addHandler(handler)
 
     def debug(
         self,
-        summary,
-        details=None,
-        channel=None,
-        force_push_slack=False,
+        summary: str,
+        details: str = None,
+        slack_channel: str = None,
+        force_push_slack: bool = False,
         *args,
         **kwargs,
-    ):
+    ) -> None:
         self._log(
             DEBUG,
             self.get_name(),
             summary,
             details,
-            channel,
+            slack_channel,
             force_push_slack,
             *args,
             **kwargs,
@@ -147,19 +148,19 @@ class AKLogger(object):
 
     def info(
         self,
-        summary,
-        details=None,
-        channel=None,
-        force_push_slack=False,
+        summary: str,
+        details: str = None,
+        slack_channel: str = None,
+        force_push_slack: bool = False,
         *args,
         **kwargs,
-    ):
+    ) -> None:
         self._log(
             INFO,
             self.get_name(),
             summary,
             details,
-            channel,
+            slack_channel,
             force_push_slack,
             *args,
             **kwargs,
@@ -167,19 +168,19 @@ class AKLogger(object):
 
     def warning(
         self,
-        summary,
-        details=None,
-        channel=None,
-        force_push_slack=False,
+        summary: str,
+        details: str = None,
+        slack_channel: str = None,
+        force_push_slack: bool = False,
         *args,
         **kwargs,
-    ):
+    ) -> None:
         self._log(
             WARNING,
             self.get_name(),
             summary,
             details,
-            channel,
+            slack_channel,
             force_push_slack,
             *args,
             **kwargs,
@@ -187,19 +188,19 @@ class AKLogger(object):
 
     def error(
         self,
-        summary,
-        details=None,
-        channel=None,
-        force_push_slack=False,
+        summary: str,
+        details: str = None,
+        slack_channel: str = None,
+        force_push_slack: bool = False,
         *args,
         **kwargs,
-    ):
+    ) -> None:
         self._log(
             ERROR,
             self.get_name(),
             summary,
             details,
-            channel,
+            slack_channel,
             force_push_slack,
             *args,
             **kwargs,
@@ -207,22 +208,22 @@ class AKLogger(object):
 
     def _log(
         self,
-        level,
-        name,
-        summary=None,
-        details=None,
-        channel=None,
-        force_push_slack=False,
+        level: str,
+        name: str,
+        summary: str = None,
+        details: str = None,
+        slack_channel: str = None,
+        force_push_slack: bool = False,
         *args,
         **kwargs,
-    ):
+    ) -> None:
         if self.parent:
             self.parent._log(
                 level,
                 name,
                 summary,
                 details,
-                channel,
+                slack_channel,
                 force_push_slack,
                 *args,
                 **kwargs,
@@ -237,20 +238,22 @@ class AKLogger(object):
         method_to_call = getattr(self.logger, _levelToName.get(level).lower())
         method_to_call(msg, *args, **kwargs)
         if force_push_slack or self.should_push_to_slack(level):
-            self.slack_push(summary, details, channel, level)
+            self.slack_push(summary, details, slack_channel, level)
 
-    def get_slack_color(self, level):
+    def get_slack_color(self, level: str) -> str:
         color = _levelToColor.get(level, _levelToColor[10])
         return color
 
-    def should_push_to_slack(self, level):
+    def should_push_to_slack(self, level: str) -> bool:
         if self.slack_client is None:
             return False
         if level < self.get_slack_level():
             return False
         return True
 
-    def slack_push(self, summary, details, channel, level):
+    def slack_push(
+        self, summary: str, details: str, channel: str, level: str
+    ) -> None:
         if not channel:
             channel = self.slack_channel
 
@@ -281,7 +284,7 @@ class AKLogger(object):
 logger = AKLogger("root")
 
 
-def getLogger(name=None):
+def getLogger(name: str = None) -> AKLogger:
     if name is None:
         return logger
     return logger.getLogger(name)
--- aklogger/handlers.py
+++ aklogger/handlers.py
@@ -5,13 +5,13 @@ from aklogger import logger
 class DjangoHandler(logging.Handler):
     def __init__(
         self,
-        name,
-        slack_token=None,
-        slack_level=logging.ERROR,
-        slack_channel="#error",
-        slack_icon_emoji=":shipit:",
-        filename=None,
-    ):
+        name: str,
+        slack_token: str = None,
+        slack_level: str = logging.ERROR,
+        slack_channel: str = "#error",
+        slack_icon_emoji: str = ":shipit:",
+        filename: str = None,
+    ) -> None:
         super().__init__()
         logger.set_name(name)
         if slack_token:
@@ -21,10 +21,10 @@ class DjangoHandler(logging.Handler):
         if filename:
             logger.log_to_file(filename)
 
-    def setLevel(self, level):
+    def setLevel(self, level: str) -> None:
         logger.setLevel(level)
         super(DjangoHandler, self).setLevel(level)
 
-    def emit(self, record):
+    def emit(self, record: logging.LogRecord) -> None:
         method_to_call = getattr(logger, record.levelname.lower())
         method_to_call(record.getMessage())
